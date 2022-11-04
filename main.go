eb3d1c5f5f0ea6b4e6b6a31ec80781a25a49454e
Kishor KC <kishor@esignature.com.np>
2022-11-04 17:20:03 +0545

    add gin framework

--- main.go
+++ main.go
@@ -1,44 +1,7 @@
-package main
+package main 
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "PraseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST successfull\n")
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Email = %s\n", email)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	log.Print("This is our first log message in Go.", w)
-	fmt.Fprintf(w, "hello")
-}
-
-func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer);
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
+func main() {}

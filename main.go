06e5388095fa60fcfc8c533c46dd04140e3beebc
Kishor KC <kishor@esignature.com.np>
2022-11-04 17:59:50 +0545

    add gin

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
-package main 
+package main
 
 import (
+	"net/http"
+	"github.com/gin-gonic/gin"
 	"fmt"
 )
 
-func main() {}
+func main() {
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"192.168.0.117"})
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	});
+
+	router.GET("/home", func(c *gin.Context) {
+		fmt.Printf("ClientIp: %s\n", c.ClientIP())
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
+}

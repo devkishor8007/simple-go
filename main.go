2766332aa55efdbde8d04e1ddffe0fbcf9e84eac
Kishor KC <kishor@esignature.com.np>
2022-11-07 16:47:17 +0545

    add multiple get request error on base url - give us empty object

--- main.go
+++ main.go
@@ -6,7 +6,28 @@ import (
 	"fmt"
 )
 
+type todo struct {
+	id string
+	title string
+	description string
+	isShare bool
+}
+
+
+
+func getTodos(c *gin.Context) {
+	var todoArray = []todo {
+		{ id: "1", title: "first", description: "this could be first", isShare: true },
+		{ id: "2", title: "second", description: "this could be second", isShare: false },
+	}
+	c.IndentedJSON(http.StatusOK, todoArray)
+}
+
+
+
 func main() {
+
+
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"192.168.0.117"})
 	router.GET("/ping", func(c *gin.Context) {
@@ -18,5 +39,17 @@ func main() {
 	router.GET("/home", func(c *gin.Context) {
 		fmt.Printf("ClientIp: %s\n", c.ClientIP())
 	})
+
+	router.GET("/home/:id", func(c *gin.Context) {
+		id := c.Params.ByName("id")
+		fmt.Printf(id);
+		c.JSON(http.StatusOK, gin.H{
+			"params": id,
+			"status": "true",
+		})
+	})
+
+	router.GET("/", getTodos)
+
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
